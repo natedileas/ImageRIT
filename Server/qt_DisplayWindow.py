c556de1ac2d4c64a6a7ef6833a361b6a68381901
Nathan Dileas <ndileas@gmail.com>
2017-04-27 12:29:28 -0400

    Added more informative statusbar messages

--- Server/qt_DisplayWindow.py
+++ Server/qt_DisplayWindow.py
@@ -13,7 +13,7 @@ from g_api_email import send_async
 
 
 class DisplayWindow(QMainWindow):
-    save_name = os.getcwd() + '\\output\\{0}.png'
+    save_name = os.getcwd() + '\\output\\TEMP.png'
 
     def __init__(self, cameraId, state_func):
         QMainWindow.__init__(self)
@@ -45,6 +45,18 @@ class DisplayWindow(QMainWindow):
 
     @QtCore.pyqtSlot(str)
     def show_msg(self, msg):
+        if 'Connected:' in msg:
+            msg += '|'+self.ui.statusBar.text()
+        elif 'Disconnected:' in msg:
+            # remove connected statement from status
+            curr = self.ui,statusBar.text().split('|')
+            new = []
+            for section in curr:
+                if msg[-5:] in section:
+                    new.append(msg)
+                else:
+                    new.append(section)
+            msg = new
         self.ui.statusBar.showMessage(msg)
 
     @QtCore.pyqtSlot(dict)
